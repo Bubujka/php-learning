287fe0abad8e8a8bc827fa2a97b33a5c440542ae
Aleksej Kamynin <zendzirou@gmail.com>
2018-02-18 10:29:48 +0300

    Несколько примеров на php

--- code/array/array_contains.php
+++ code/array/array_contains.php
@@ -3,3 +3,9 @@
 /*
 Проверить что в массиве есть значение
 */
+
+$arr = ['bubujka', 'zuzujka'];
+var_dump(in_array('bubujka', $arr));
+var_dump(in_array('jujujka', $arr));
+
+
--- code/array/remove_dict_prop.php
+++ code/array/remove_dict_prop.php
@@ -3,3 +3,8 @@
 /*
 Удалить свойство из объекта
 */
+
+$arr = ['name'=>'bubujka', 'age'=>28];
+unset($arr['name']);
+print_r($arr);
+
