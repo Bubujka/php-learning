36dd91627a3089e39764249f65b4df6bc3845c55
Aleksej Kamynin <zendzirou@gmail.com>
2018-04-07 14:50:19 +0300

    Пара примеров

--- code/array/map.php
+++ code/array/map.php
@@ -3,3 +3,8 @@
 /*
 Преобразовать один массив в другой
 */
+
+$arr = [1,2,3];
+
+$arr2 = array_map(function($v){ return $v*$v; }, $arr);
+print_R($arr2);
--- code/array/shift_pop.php
+++ code/array/shift_pop.php
@@ -3,3 +3,12 @@
 /*
 Вытаскивание элементов в массиве с начала, с конца
 */
+
+$arr = [0, 'a', 'b', 'c', 5];
+
+echo array_shift($arr);
+echo array_pop($arr);
+
+print_R($arr);
+
+
